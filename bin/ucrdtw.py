3864fd2a3482c060f5b081c3c40c6ae2648d1214
gwb84 <gregory.bartram@gmail.com>
2018-12-14 15:40:06 -0500

    added exclusion zone

--- bin/ucrdtw.py
+++ bin/ucrdtw.py
@@ -11,10 +11,11 @@ if __name__ == '__main__':
     parser.add_argument('query_size', metavar='QUERY_SIZE', type=int, default=0, help='Max size of query')
     parser.add_argument('warp_width', metavar='WARP_WIDTH', type=float, default=0.05, help='Width of allowed warp as fraction of query size')
     parser.add_argument('curr_ind', metavar='CURR_IND', type=int, default=-1, help='Current index for self similarity')
+    parser.add_argument('ez', metavar='EZ', type=int, default=2, help='Exclusion Zone for self similarity')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Print verbose info')
     args = parser.parse_args()
 
     data = numpy.fromfile(args.data, sep=' ')
     query = numpy.fromfile(args.query, sep=' ', count=args.query_size if args.query_size > 0 else -1)
-    print(_ucrdtw.ucrdtw(data, query, args.warp_width, args.curr_ind, True))
+    print(_ucrdtw.ucrdtw(data, query, args.warp_width, args.curr_ind, args.ez, True))
 
